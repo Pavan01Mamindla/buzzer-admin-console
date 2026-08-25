a2eba595d4d733a2e3f692fea79a116b8e520885
Pavan Mamindla <pavanmamindla1@gmail.com>
2026-08-25 22:16:51 +0530

    chore: configure app foundation

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    apiUrl: 'https://backend-3ofw.onrender.com'
+  };
+  
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://backend-3ofw.onrender.com'
+  };
+  
